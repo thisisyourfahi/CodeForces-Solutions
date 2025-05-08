d19b5f907767bf500489d7cbfb7440e89cb83767
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2025-05-08 22:48:04 +0600

    greedy imp

---
+++ Difficulty 800/1991A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    int mx = INT_MIN;
+    for (int i = 0; i < n; i += 2) {
+        mx = max(mx, a[i]);
+    }
+    cout << mx << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
