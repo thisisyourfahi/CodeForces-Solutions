1f73a222b520c2b1e654cd156b83a09f843b7de2
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2025-07-26 11:23:17 +0600

    brute force

---
+++ Difficulty 900/1875A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll> 
+#define f(i, n) for (int i = 0; i < n; i++) 
+#define all(v) v.begin(), v.end()
+
+void solve() {
+    int a, b, n;
+    cin >> a >> b >> n;
+    vi v(n);
+    f(i, n) cin >> v[i];
+
+    ll ans = b;
+    f(i, n) {
+        ans += min(a - 1, v[i]);
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
